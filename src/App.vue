64943feb31837a790b3cdff40b708e724af783a0
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-05-28 21:42:50 +0200

    chroe: added first version of the interface

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+import { pokedex } from "/pokedex.js";
+
+const selectedGens = ref([]);
+const selectedLanguage = ref("fr");
+
+const gensList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const languageList = ["fr", "eng", "jp"];
+const pokemonList = ref([]);
+const canPrint = ref(false);
+
+onMounted(() => {
+	selectedGens.value = gensList;
+});
+
+// Generate the filtered list
+function generate() {
+	if (selectedGens.value.length === 0) {
+		selectedGens.value = gensList;
+	}
+	pokemonList.value = pokedex
+		.filter((pokemon) => selectedGens.value.includes(pokemon.generation)) // Filter by generation
+		.map((pokemon) => ({
+			id: pokemon.id,
+			name: pokemon.name[selectedLanguage.value],
+			imageUrl: `Printedex/sprites/default/${pokemon.spriteFilenames.default}`,
+			generation: pokemon.generation,
+		}));
+	canPrint.value = true;
+}
+
+// Show print dialog
+function showPrintDialog() {
+	window.print();
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+	<div class="generate">
+		<!-- User selection controls -->
+		<div id="filters">
+			<div>
+				<fieldset>
+					<legend>Choisissez une ou plusieurs générations</legend>
+					<label
+						v-for="gen in gensList"
+						:key="gen"
+					>
+						<input
+							type="checkbox"
+							:value="gen"
+							v-model="selectedGens"
+						/>
+						Gen {{ gen }}
+					</label>
+				</fieldset>
+			</div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+			<div>
+				<fieldset>
+					<legend>Choisissez une langue pour les noms</legend>
+					<label
+						v-for="lang in languageList"
+						:key="lang"
+					>
+						<input
+							type="radio"
+							:value="lang"
+							v-model="selectedLanguage"
+						/>
+						{{ lang }}
+						<span>
+							<img
+								class="flag-icon"
+								:src="`Printedex/icons/${lang}-flag.svg`"
+								alt=""
+							/>
+						</span>
+					</label>
+				</fieldset>
+			</div>
+		</div>
+		<button @click="generate">Generate List</button>
+		<button
+			:disabled="!canPrint"
+			@click="showPrintDialog"
+		>
+			Impression
+		</button>
+	</div>
+
+	<!-- generated list -->
+	<div
+		id="container"
+		class="container"
+	>
+		<div
+			v-for="pokemon in pokemonList"
+			:key="pokemon.id"
+			class="cell"
+		>
+			<input
+				type="checkbox"
+				v-model="pokemon.selected"
+			/>
+			<img
+				class="sprite"
+				:src="pokemon.imageUrl"
+				:alt="pokemon.name"
+			/>
+			<div class="id-name-wrapper">
+				<p class="pokemon-id">#{{ pokemon.id }} - G{{ pokemon.generation }}</p>
+				<p>{{ pokemon.name }}</p>
+			</div>
+		</div>
+	</div>
+</template>
